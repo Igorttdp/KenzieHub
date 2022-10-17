b961fda3a2131887f61b6f3462af10f96fa61509
Igorttdp <igorttdp.dev@gmail.com>
2022-10-17 11:02:04 -0300

    feat: delete techs are avaliable

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
+const AUTH_TOKEN = localStorage.getItem("@kenziehub__token");
+
 const api = axios.create({
   baseURL: "https://kenziehub.herokuapp.com",
   timeout: 5000,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
+
+api.defaults.headers.common["Content-Type"] = "application/json";
+api.defaults.headers.common["Authorization"] = `Bearer ${AUTH_TOKEN}`;
 
 export default api;
