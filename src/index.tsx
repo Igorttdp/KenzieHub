cc3c243ffa6dcfb0de2ab79d59343b7a4e7f6e60
Igorttdp <igorttdp.dev@email.com>
2023-02-28 19:57:29 -0300

    feat: add imports comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
+// React
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+// Components
 import GlobalStyle from "./components/global";
+
+// React Router Dom
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
