5fcad9fd47ad735270a45945b0746cefb89c1895
Igorttdp <igorttdp.dev@email.com>
2023-02-28 15:38:21 -0300

    feat: move handle submit function to a dedicated Form component

---
+++ src/pages/Dashboard/Components/Techs/TechsForm.tsx
@@ -0,0 +1,63 @@
+import { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import {
+  IAddTechsDataProps,
+  TechsContext,
+} from "../../../../context/Technologies/TechsContext";
+import CustomForm from "../Modal/CustomForm";
+
+export interface iTechsProps {
+  handleClose: () => void;
+}
+
+const TechsForm = ({ handleClose }: iTechsProps) => {
+  const { addTech } = useContext(TechsContext);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState,
+    formState: { isSubmitSuccessful },
+  } = useForm<IAddTechsDataProps>();
+
+  const onFormSubmit = () => {
+    handleSubmit(addTech);
+    handleClose();
+  };
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({
+        title: "",
+        status: "",
+      });
+    }
+  }, [formState, isSubmitSuccessful, reset]);
+
+  return (
+    <CustomForm onSubmit={onFormSubmit}>
+      <div>
+        <label htmlFor="name">Nome</label>
+        <input
+          type="text"
+          id="name"
+          placeholder="Ex: Javascript"
+          required
+          {...register("title")}
+        />
+      </div>
+      <div>
+        <label htmlFor="status">Selecionar status</label>
+        <select id="status" {...register("status")}>
+          <option value="Iniciante">Iniciante</option>
+          <option value="Intermediário">Intermediário</option>
+          <option value="Avançado">Avançado</option>
+        </select>
+      </div>
+      <button>Cadastrar Tecnologia</button>
+    </CustomForm>
+  );
+};
+
+export default TechsForm;
