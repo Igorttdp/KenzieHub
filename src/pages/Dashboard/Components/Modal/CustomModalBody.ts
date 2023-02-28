6b5c7b886115ce6b85b69aaf7a1c3ef8d131b592
Igorttdp <igorttdp.dev@email.com>
2023-02-28 15:33:20 -0300

    feat: add custom styled component for 'ModalBody' bootstrap component

---
+++ src/pages/Dashboard/Components/Modal/CustomModalBody.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { ModalBody } from "react-bootstrap";
+
+const CustomModalBody = styled(ModalBody)`
+  padding: 0;
+  height: 30rem;
+  background-color: #212529;
+`
+
+export default CustomModalBody;
